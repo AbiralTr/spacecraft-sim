17db2fa88d190f9b1dd62d52b339856ac822ab50
AbiralTr <abiraltuladhar22@gmail.com>
2026-07-09 18:20:13 -0600

    Added circular, included, two body orbit propagation math

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from spacecraft import Spacecraft
 from groundstation import GroundStation
+import math
 
-s1 = Spacecraft("GAMMA-1")
+s1 = Spacecraft("GAMMA-1", 400, 90.5)
 sc_list = [s1]
 geodetic_position = [101, 23, 1000]
 g1 = GroundStation("Atlantis", geodetic_position, sc_list)
